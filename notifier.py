622f21641f648394d159210453291371a29f9177
senufo <fce.valeins@libertysurf.fr>
2011-11-17 12:56:45 +0100

    Nettoyage du code

--- notifier.py
+++ notifier.py
@@ -27,7 +27,6 @@ NbMsg = [0,0,0,0]
 numEmails = 0 
 #ID du screen HOME
 homeWin = xbmcgui.Window(WINDOW_HOME)
-currentWindowId = xbmcgui.getCurrentWindowId()
 #On stocke le temps courant
 temps = time.clock()
 #On prend l'intervalle entre deux releve de boite
@@ -44,65 +43,59 @@ MsgTot = True
 NoServ = 1
 
 def Checkmail(USER,NOM,SERVER,PASSWORD,PORT,SSL,TYPE,MsgTot,NoServ):
-		print "CHECKMAIL"
 		msg = ''
                 NxMsgTot = 0
                 numEmails = 0 
 		i = NoServ
-		print "Ligne 55"
-                #Teste si USER existe 
-		if (USER != ''):
-		   try:
-			#Partie POP3
-			if  '0' in TYPE:  #'POP' 
-			  if SSL:
-			       mail = poplib.POP3_SSL(str(SERVER),int(PORT))
-			  else:  #'POP3'
-			       mail = poplib.POP3(str(SERVER),int(PORT))
-                          mail.user(str(USER))
-		          mail.pass_(str(PASSWORD))
-	        	  numEmails = mail.stat()[0]
-                        #Partie IMAP
-                        if '1' in TYPE:
-			  if SSL:
-			       imap = imaplib.IMAP4_SSL(SERVER, int(PORT))
-	                  else:
-		               imap = imaplib.IMAP4(SERVER, int(PORT))
-	  		  imap.login(USER, PASSWORD)
-                       	  id = 'folder' + str(i)
-		          FOLDER = Addon.getSetting( id )
-		          imap.select(FOLDER)
-	                #  numEmails = 1 
-	                  numEmails = len(imap.search(None, 'UnSeen')[1][0].split()) 
-		          #print "IMAP numEmails = %d " % numEmails
-                   except:
-		   	locstr = Addon.getLocalizedString(id=613) #Erreur de connection
-			if Addon.getSetting( 'erreur' ) == "true":
-                        	xbmc.executebuiltin("XBMC.Notification(%s :, %s,30)" % (locstr,SERVER))
-		        #print "Erreur de connection : %s, erreur = %s" % (SERVER, mail)
-		        msg = msg + "%s : %s\n" % (NOM,locstr)
+		#Connection au serveur
+		try:
+		   #Partie POP3
+		   if  '0' in TYPE:  #'POP' 
+		       if SSL:
+		           mail = poplib.POP3_SSL(str(SERVER),int(PORT))
+		       else:  #'POP3'
+		           mail = poplib.POP3(str(SERVER),int(PORT))
+                       mail.user(str(USER))
+		       mail.pass_(str(PASSWORD))
+	               numEmails = mail.stat()[0]
+                   #Partie IMAP
+                   if '1' in TYPE:
+		      if SSL:
+		          imap = imaplib.IMAP4_SSL(SERVER, int(PORT))
+	              else:
+		          imap = imaplib.IMAP4(SERVER, int(PORT))
+	  	      imap.login(USER, PASSWORD)
+                      id = 'folder' + str(i)
+		      FOLDER = Addon.getSetting( id )
+		      imap.select(FOLDER)
+	              numEmails = len(imap.search(None, 'UnSeen')[1][0].split()) 
+		      #print "IMAP numEmails = %d " % numEmails
+                except:
+		      locstr = Addon.getLocalizedString(id=613) #Erreur de connection
+		      if Addon.getSetting( 'erreur' ) == "true":
+                           xbmc.executebuiltin("XBMC.Notification(%s :, %s,30)" % (locstr,SERVER))
+		      #print "Erreur de connection : %s, erreur = %s" % (SERVER, mail)
+		      msg = msg + "%s : %s\n" % (NOM,locstr)
 
-		   if numEmails > 0: MsgTot = True #Il y a des messages
-		   #On regarde si un nouveau mail est arrive
-	           if NbMsg[i] == 0: NbMsg[i] = numEmails
-		   NxMsg = numEmails - NbMsg[i]
-		   if NxMsg > 0 : 
-			NbMsg[i] = NbMsg[i] + NxMsg
-			NxMsgTot = NxMsgTot + NxMsg
-	           else:
-			NbMsg[i] = numEmails
-		   #print "numEmails = %d " % numEmails
-		   locstr = Addon.getLocalizedString(id=610) #messages(s)
-		   if numEmails != 0:
-		          #msg = msg + "%s : %d %s" % (NOM,numEmails, locstr) + "\n"
-		          msg = msg + "%s : %d " % (NOM,numEmails) + "\n"
-		          numEmails = 0
-                   if NxMsgTot > 0:
+		if numEmails > 0: MsgTot = True #Il y a des messages
+		#On regarde si un nouveau mail est arrive
+	        if NbMsg[i] == 0: NbMsg[i] = numEmails
+		NxMsg = numEmails - NbMsg[i]
+		if NxMsg > 0 : 
+		    NbMsg[i] = NbMsg[i] + NxMsg
+		    NxMsgTot = NxMsgTot + NxMsg
+	        else:
+		    NbMsg[i] = numEmails
+		locstr = Addon.getLocalizedString(id=610) #messages(s)
+		if numEmails != 0:
+		      msg = msg + "%s : %d " % (NOM,numEmails) + "\n"
+		      numEmails = 0
+                if NxMsgTot > 0:
 	       	      locstr = Addon.getLocalizedString(id=611) #Nouveau(x) message(s)
                       #Affiche un popup a l'arrivee d'un nx msg
 		      if Addon.getSetting( 'popup' ) == "true":
 		            xbmc.executebuiltin("XBMC.Notification( ,%d %s,60)" % (NxMsgTot,locstr))
-	           return MsgTot,msg
+	        return MsgTot,msg
 
 
 #Verifie que xbmc tourne
@@ -112,9 +105,7 @@ while (not xbmc.abortRequested):
 	  #un temps >  a l'intervalle
           temps =  time.clock() 
 	  if (temps > (old_temps + intervalle)):
-            #print "temps_sup = %f, old_temps = %f" % (temps, old_temps)
 	    old_temps = temps
-	    #print "time %f " % temps
             #On vide le message
             msg = ''
 	    message = ''
@@ -133,7 +124,8 @@ while (not xbmc.abortRequested):
 	     PORT =  Addon.getSetting( 'port' + str(NoServ))
 	     SSL = Addon.getSetting( 'ssl' + str(NoServ)) == "true"
 	     TYPE = Addon.getSetting( 'type' + str(NoServ))
-	     print "=>SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s, TYPE = %s" % (SERVER,PORT,USER, PASSWORD, SSL,TYPE)
+	     #print "=>SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s, TYPE = %s" % (SERVER,PORT,USER, PASSWORD, SSL,TYPE)
+	     #Si pas d'utilisateur on ne releve pas les msgs
              if (USER != ''):
                (MsgTot, msg) = Checkmail(USER,NOM,SERVER,PASSWORD,PORT,SSL,TYPE, MsgTot,NoServ)
 	     message = message + msg
@@ -150,6 +142,7 @@ while (not xbmc.abortRequested):
 		SSL = Addon.getSetting( 'ssl' + str(i) ) == "true"
 		TYPE = Addon.getSetting( 'type' + str(i) )
 		#print "SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s, TYPE = %s" % (SERVER,PORT,USER, PASSWORD, SSL,TYPE)
+	        #Si pas d'utilisateur on ne releve pas les msgs
                 if (USER != ''):
 		   (MsgTot, msg) = Checkmail(USER,NOM,SERVER,PASSWORD,PORT,SSL,TYPE, MsgTot,i)
 		message = message + msg

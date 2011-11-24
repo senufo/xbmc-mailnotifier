998640de28baabf18ca697166dd51af6afd61e95
senufo <fce.valeins@libertysurf.fr>
2011-11-24 15:16:29 +0100

    indent correction

--- notifier.py
+++ notifier.py
@@ -9,36 +9,37 @@ __author__     = "Senufo"
 __scriptid__   = "service.notifier"
 __scriptname__ = "Notifier"
 
-Addon      = xbmcaddon.Addon(__scriptid__)
+Addon          = xbmcaddon.Addon(__scriptid__)
 
 __cwd__        = Addon.getAddonInfo('path')
 __version__    = Addon.getAddonInfo('version')
 __language__   = Addon.getLocalizedString
 
-__profile__    = xbmc.translatePath( Addon.getAddonInfo('profile') )
-__resource__   = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
+__profile__    = xbmc.translatePath(Addon.getAddonInfo('profile'))
+__resource__   = xbmc.translatePath(os.path.join(__cwd__, 'resources',
+                                                 'lib'))
 
 #sys.path.append (__resource__)
 
 
 
 #ID de la fenetre HOME
-WINDOW_HOME = 10000 
+WINDOW_HOME = 10000
 
 #Variables generales
 msg = ''
-NbMsg = [0,0,0,0]
-numEmails = 0 
+NbMsg = [0, 0, 0, 0]
+numEmails = 0
 #No serveur
 NoServ = 1
 #Position du texte
-x = int(Addon.getSetting( 'x' ))
-y = int(Addon.getSetting( 'y' ))
-width = int(Addon.getSetting( 'width' ))
-height = int(Addon.getSetting( 'height' ))
-font = Addon.getSetting( 'font' )
-color = Addon.getSetting( 'color' )
-ALT =  Addon.getSetting( 'alt' )
+x = int(Addon.getSetting('x'))
+y = int(Addon.getSetting('y'))
+width = int(Addon.getSetting('width'))
+height = int(Addon.getSetting('height'))
+font = Addon.getSetting('font')
+color = Addon.getSetting('color')
+ALT = Addon.getSetting('alt')
 print "ALT +> %s " % ALT
 MsgBox = None
 MsgBoxId = None
@@ -48,24 +49,24 @@ re_added_control = False
 #Verifie que xbmc tourne
 while (not xbmc.abortRequested):
     #Attente avant de relever les mails
-    intervalle = int( float( Addon.getSetting( 'time' ) ) * 60.0 )
-    if start_time and ( time.time() - start_time ) < intervalle:
-        time.sleep( .5 )
-        SHOW_UPDATE     = Addon.getSetting( 'show_update' ) == "true"
+    intervalle = int(float(Addon.getSetting('time')) * 60.0)
+    if start_time and (time.time() - start_time) < intervalle:
+        time.sleep(.5)
+        SHOW_UPDATE     = Addon.getSetting('show_update') == "true"
         if MsgBox:
             # optional show le temps qu'il reste avant la prochaine MAJ
             try:
-	        if SHOW_UPDATE:
-         	        up = int( intervalle ) - ( time.time() - start_time )
-                        locstr = Addon.getLocalizedString(615) #Update in %i second
-                        print "MSG up = %s " % msg
-                        label = "%s[CR]" %  msg + locstr % up
-		else:  #Il faut rafraichir l'affchage
-                        print "MSG = %s " % msg
-			label = '%s' % msg
+                if SHOW_UPDATE:
+                    up = int(intervalle) - (time.time() - start_time)
+                    locstr = Addon.getLocalizedString(615)  #Update in %i second
+                    #print "MSG up = %s " % msg
+                    label = "%s[CR]" %  msg + locstr % up
+                else: #Il faut rafraichir l'affichage
+                    #print "MSG = %s " % msg
+                    label = '%s' % msg
                 MsgBox.setLabel( label )
             except Exception, e:
-                print str( e )
+                print str(e)
 
         HomeNotVisible = xbmc.getCondVisibility( "!Window.IsVisible(10000)" )
         if HomeNotVisible:
@@ -114,9 +115,9 @@ while (not xbmc.abortRequested):
         SSL      = Addon.getSetting( 'ssl%i'    % NoServ )
         TYPE     = Addon.getSetting( 'type%i'   % NoServ )
         FOLDER   = Addon.getSetting( 'folder%i' % NoServ )
-        
+
 #        print "SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s" % (SERVER,PORT,USER, PASSWORD, SSL)
-	       
+
     #On recupere les parametres des trois serveurs
     for i in range( 1, 4 ): #[1,2,3]:
         if Addon.getSetting( 'enableserver%i' % i ) == "false":
@@ -130,38 +131,38 @@ while (not xbmc.abortRequested):
         SSL      = Addon.getSetting( 'ssl%i'    % i )
         TYPE     = Addon.getSetting( 'type%i'   % i )
         FOLDER   = Addon.getSetting( 'folder%i' % i )
-        
-        print "SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s" % (SERVER,PORT,USER, PASSWORD, SSL)
-	#Total des nx messages
-	NxMsgTot = 0
-	#Pas de nx message
-	MsgTot = False
 
-        # Teste si USER existe 
+        print "SERVER = %s, PORT = %s, USER = %s, password = %s, SSL = %s" % (SERVER, PORT, USER, PASSWORD, SSL)
+#Total des nx messages
+        NxMsgTot = 0
+#Pas de nx message
+        MsgTot = False
+
+# Teste si USER existe
         if (USER != ''):
-            try :
+            try:
                 locstr = Addon.getLocalizedString(616) #Releve du courrier
                 MsgBox.setLabel( locstr % NOM )
-                #Partie POP3
-		if  '0' in TYPE:  #'POP' 
-		  if SSL: 
-                    mail = poplib.POP3(str(SERVER),int(PORT))
-                  else: 
-                    mail = poplib.POP3_SSL(str(SERVER),int(PORT))
-                  mail.user(str(USER))
-                  mail.pass_(str(PASSWORD))
-                  numEmails = mail.stat()[0]
-		#Partie IMAP
+#Partie POP3
+                if  '0' in TYPE:  #'POP'
+                    if SSL:
+                        mail = poplib.POP3(str(SERVER), int(PORT))
+                    else:
+                        mail = poplib.POP3_SSL(str(SERVER), int(PORT))
+                    mail.user(str(USER))
+                    mail.pass_(str(PASSWORD))
+                    numEmails = mail.stat()[0]
+#Partie IMAP
                 if '1' in TYPE:
-		  if SSL:
-		    imap = imaplib.IMAP4_SSL(SERVER, int(PORT))
-	          else:
-		    imap = imaplib.IMAP4(SERVER, int(PORT))
-	  	  imap.login(USER, PASSWORD)
-		  FOLDER = Addon.getSetting( 'folder%i' % i )
-		  imap.select(FOLDER)
-	          numEmails = len(imap.search(None, 'UnSeen')[1][0].split()) 
-		  print "IMAP numEmails = %d " % numEmails
+                    if SSL:
+                        imap = imaplib.IMAP4_SSL(SERVER, int(PORT))
+                    else:
+                        imap = imaplib.IMAP4(SERVER, int(PORT))
+                    imap.login(USER, PASSWORD)
+                    FOLDER = Addon.getSetting( 'folder%i' % i )
+                    imap.select(FOLDER)
+                    numEmails = len(imap.search(None, 'UnSeen')[1][0].split())
+                    print "IMAP numEmails = %d " % numEmails
 
                 print "numEmails = %d " % numEmails
                 locstr = Addon.getLocalizedString(610) #message(s)
@@ -170,39 +171,41 @@ while (not xbmc.abortRequested):
             except:
                 locstr = Addon.getLocalizedString(613) #erreur de connection
                 if Addon.getSetting( 'erreur' ) == "true":
-                     xbmc.executebuiltin("XBMC.Notification(%s : ,%s,30)" % (locstr,SERVER))
+                    xbmc.executebuiltin("XBMC.Notification(%s : ,%s,30)" % (locstr, SERVER))
                 print "Erreur de connection : %s" % SERVER
-		#Msg affiche sur le HOME
-                msg = msg + "%s : %s\n" % (NOM,locstr)
+#Msg affiche sur le HOME
+                msg = msg + "%s : %s\n" % (NOM, locstr)
 
-            if numEmails > 0: MsgTot = True #Il y a des messages
+            if numEmails > 0:
+                MsgTot = True #Il y a des messages
             #On regarde si un nouveau mail est arrive
-	    if NbMsg[i] == 0: NbMsg[i] = numEmails
-	    print "NbMsg %d " % NbMsg[i]
-	    NxMsg = numEmails - NbMsg[i]
-	    print "NxMsg = %d " %NxMsg
-	    if NxMsg > 0 : 
-	         NbMsg[i] = NbMsg[i] + NxMsg
-	         NxMsgTot = NxMsgTot + NxMsg
-	    else:
-	         NbMsg[i] = numEmails
-	    print "NxMsgTot = %d, NbMsg = %d" % (NxMsgTot,NbMsg[i])
-	    locstr = Addon.getLocalizedString(id=610) #messages(s)
-	    if numEmails != 0:
-		if (ALT and i == NoServ):
-		   print "ALT %s " % NOM
-                   msg = "%s : %d " % (NOM,numEmails) + "\n"
-		elif not ALT:
-                   print "!ALT %s " % NOM
-	           msg = msg + "%s : %d " % (NOM,numEmails) + "\n"
-		numEmails = 0
+            if NbMsg[i] == 0:
+                NbMsg[i] = numEmails
+            print "NbMsg %d " % NbMsg[i]
+            NxMsg = numEmails - NbMsg[i]
+            print "NxMsg = %d " % NxMsg
+            if NxMsg > 0:
+                NbMsg[i] = NbMsg[i] + NxMsg
+                NxMsgTot = NxMsgTot + NxMsg
+            else:
+                NbMsg[i] = numEmails
+            print "NxMsgTot = %d, NbMsg = %d" % (NxMsgTot, NbMsg[i])
+            locstr = Addon.getLocalizedString(id=610) #messages(s)
+            if numEmails != 0:
+                if (ALT and i == NoServ):
+                    print "ALT %s " % NOM
+                    msg = "%s : %d " % (NOM, numEmails) + "\n"
+            elif not ALT:
+                print "!ALT %s " % NOM
+                msg = msg + "%s : %d " % (NOM, numEmails) + "\n"
+            numEmails = 0
             if NxMsgTot > 0:
-	          locstr = Addon.getLocalizedString(id=611) #Nouveau(x) message(s)
-                  #Affiche un popup a l'arrivee d'un nx msg
-	          if Addon.getSetting( 'popup' ) == "true":
-	              xbmc.executebuiltin("XBMC.Notification( ,%d %s sur %s,160)" % (NxMsgTot,locstr,NOM))
+                locstr = Addon.getLocalizedString(id=611) #Nouveau(x) message(s)
+                #Affiche un popup a l'arrivee d'un nx msg
+                if Addon.getSetting( 'popup' ) == "true":
+                    xbmc.executebuiltin("XBMC.Notification( ,%d %s sur %s,160)" % (NxMsgTot, locstr, NOM))
     NoServ += 1  #On passe au serveur suivant
-    if (NoServ > 3): NoServ =1 #Si dernier serveur on revient au premier 
+    if (NoServ > 3): NoServ = 1 #Si dernier serveur on revient au premier
     MsgBox.setLabel( msg )
 
     #initialise start time
@@ -211,4 +214,4 @@ while (not xbmc.abortRequested):
 
 
 def getNbMails():
-     print "getNbMails()"
+    print "getNbMails()"
